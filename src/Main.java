d8c1e2a016bf03d3769de95a77857e58dca22e9d
maxim1770 <maxim1770@gmail.com>
2022-10-07 10:18:59 +0300

    feat(list): добавить метод get_elem, который возвращает элемент List по индексу
    Так же добавить "тестирования" этого метода в класс Main.

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,14 @@ class List {
     private Node head;
     private Node tail;
 
-    void add_front(int data)
-    {
+    void add_front(int data) {
         Node a = new Node();
         a.data = data;
 
-        if(head == null)
-        {
+        if (head == null) {
             head = a;
             tail = a;
-        }
-        else {
+        } else {
             a.next = head;
             head = a;
         }
@@ -26,8 +23,7 @@ class List {
     void add_back(int data) {
         Node a = new Node();
         a.data = data;
-        if (tail == null)
-        {
+        if (tail == null) {
             head = a;
             tail = a;
         } else {
@@ -36,20 +32,36 @@ class List {
         }
     }
 
-    void print_all()
-    {
+    void print_all() {
         Node t = this.head;
-        while (t != null)
-        {
+        while (t != null) {
             System.out.print(t.data + " ");
             t = t.next;
         }
         System.out.println();
     }
 
-    void del_elem(int data)
-    {
-        if(head == null)
+    int get_elem(int index) {
+        if (head == null)
+            return -1;
+
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            if (node != null)
+                node = node.next;
+            else
+                return -1;
+        }
+
+        if (node != null)
+            return node.data;
+        else
+            return -1;
+
+    }
+
+    void del_elem(int data) {
+        if (head == null)
             return;
 
         if (head == tail) {
@@ -66,8 +78,7 @@ class List {
         Node t = head;
         while (t.next != null) {
             if (t.next.data == data) {
-                if(tail == t.next)
-                {
+                if (tail == t.next) {
                     tail = t;
                 }
                 t.next = t.next.next;
@@ -80,14 +91,22 @@ class List {
 
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         List l = new List();
-        for(int i = 0; i<=100; i++){
-            l.add_back(i);
+
+        System.out.println(l.get_elem(1));
+
+        for (int i = 0; i <= 100; i++) {
+            l.add_back(i + 1);
         }
 
         l.print_all();
+
+        System.out.println(l.get_elem(0));
+        System.out.println(l.get_elem(4));
+        System.out.println(l.get_elem(100));
+        System.out.println(l.get_elem(101));
+
 
         l.add_back(123);
         l.add_back(123);
@@ -97,6 +116,7 @@ public class Main {
 
         l.del_elem(2);
         l.del_elem(11);
+
 
         l.print_all();
     }
